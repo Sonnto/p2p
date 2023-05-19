512004f2f634a2739373b294760fa4016e42a370
Sonnto <90372802+Sonnto@users.noreply.github.com>
2023-05-19 15:44:53 -0400

    stylize and add functionality to upload image

--- frontend/src/components/Pixelate.jsx
+++ frontend/src/components/Pixelate.jsx
@@ -1,9 +1,35 @@
 import React from "react";
+import { fabric } from "fabric";
 
 function Pixelate() {
+  const handleImageUpload = (event) => {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+
+    reader.onload = (e) => {
+      const image = new Image();
+      image.onload = () => {
+        const canvas = new fabric.Canvas("canvas");
+
+        const fabricImage = new fabric.Image(image);
+        canvas.add(fabricImage);
+
+        // Perform pixelation logic on the fabricImage object here
+        // You can use the fabric.js API to manipulate the image
+
+        canvas.renderAll();
+      };
+
+      image.src = e.target.result;
+    };
+
+    reader.readAsDataURL(file);
+  };
+
   return (
     <div>
       <h1>The Pixelation magic component will happen here</h1>
+      <input type="file" onChange={handleImageUpload} />
     </div>
   );
 }
