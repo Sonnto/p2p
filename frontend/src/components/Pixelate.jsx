bbd177638582c6cd7c847e1f49ac9cf493bc3434
Sonnto <90372802+Sonnto@users.noreply.github.com>
2023-05-19 17:40:05 -0400

    Update Pixelate.jsx

--- frontend/src/components/Pixelate.jsx
+++ frontend/src/components/Pixelate.jsx
@@ -1,7 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 
 const Pixelate = () => {
   const [selectedImage, setSelectedImage] = useState(null);
+  const [dragPosition, setDragPosition] = useState({ x: 0, y: 0 });
+  const imageRef = useRef(null);
+  const frameSize = 500; // Adjust the frame size as needed
 
   const handleImageUpload = (event) => {
     const file = event.target.files[0];
@@ -17,12 +20,25 @@ const Pixelate = () => {
   const handlePixelate = () => {
     if (!selectedImage) return;
 
+    // Calculate the cropping dimensions based on the drag position
+    const cropX = Math.max(0, -dragPosition.x);
+    const cropY = Math.max(0, -dragPosition.y);
+    const cropSize = Math.min(
+      frameSize,
+      imageRef.current.width - cropX,
+      imageRef.current.height - cropY
+    );
+
+    // Crop the selected image based on the calculated dimensions
+    const croppedImage = cropImage(selectedImage, cropX, cropY, cropSize);
+
+    // Proceed with sending the cropped image to the backend API for pixelation
     fetch("/api/convert", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ selectedImage, pixelSize: 10 }),
+      body: JSON.stringify({ selectedImage: croppedImage, pixelSize: 10 }),
     })
       .then((response) => response.json())
       .then((data) => {
@@ -35,14 +51,68 @@ const Pixelate = () => {
       });
   };
 
+  const handleDragStart = (event) => {
+    event.dataTransfer.setData("text/plain", ""); // Required for drag operation to work properly
+  };
+
+  const handleDragOver = (event) => {
+    event.preventDefault();
+  };
+
+  const handleDrag = (event) => {
+    const { movementX, movementY } = event;
+    setDragPosition((prevPosition) => ({
+      x: prevPosition.x + movementX,
+      y: prevPosition.y + movementY,
+    }));
+  };
+
+  const cropImage = (imageSrc, x, y, size) => {
+    const canvas = document.createElement("canvas");
+    canvas.width = size;
+    canvas.height = size;
+
+    const context = canvas.getContext("2d");
+    context.drawImage(imageRef.current, x, y, size, size, 0, 0, size, size);
+
+    return canvas.toDataURL("image/jpeg");
+  };
+
+  useEffect(() => {
+    setDragPosition({ x: 0, y: 0 });
+  }, [selectedImage]);
+
   return (
     <div>
       {/* Image upload UI */}
       <input type="file" onChange={handleImageUpload} />
 
-      {/* Image preview */}
+      {/* Image preview with drag functionality */}
       {selectedImage && (
-        <img src={selectedImage} alt="Preview" width={500} height={500} />
+        <div
+          className="image-preview"
+          draggable
+          onDragStart={handleDragStart}
+          onDragOver={handleDragOver}
+          onDrag={handleDrag}
+          style={{
+            width: frameSize,
+            height: frameSize,
+            overflow: "hidden",
+            border: "1px solid black",
+          }}
+        >
+          <img
+            ref={imageRef}
+            src={selectedImage}
+            alt="Preview"
+            width={frameSize}
+            height={frameSize}
+            style={{
+              transform: `translate(${dragPosition.x}px, ${dragPosition.y}px)`,
+            }}
+          />
+        </div>
       )}
 
       {/* Pixelate button */}
