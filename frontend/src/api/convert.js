6804405fa50f961a2b801b324b32bdcd6fed9fd3
Sonnto <90372802+Sonnto@users.noreply.github.com>
2023-05-20 16:11:04 -0400

    Update convert.js

--- frontend/src/api/convert.js
+++ frontend/src/api/convert.js
@@ -1,5 +1,11 @@
-export const pixelateImage = (selectedImage, pixelSize) => {
+const sharp = require("sharp");
+
+const pixelateImage = async (selectedImage, pixelSize) => {
   //pixelate logic here
+  //decode image
+  const decodedImage = await sharp(Buffer.from(selectedImage, "base64"))
+    .toFormat("png") //conver to png for processing
+    .toBuffer();
 };
 
 module.exports = {
