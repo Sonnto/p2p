b4499b29a20f29849101624b3703d6b6a891bd72
Sonnto <90372802+Sonnto@users.noreply.github.com>
2023-06-08 13:35:16 -0400

    create backend

--- backend/server/index.js
+++ backend/server/index.js
@@ -1,13 +1,21 @@
+const path = require("path");
 const express = require("express");
 
 const PORT = process.env.PORT || 1225;
 
 const app = express();
 
-//api endpoint route
+//Node serve files for React frontend
+app.use(express.static(path.resolve(__dirname, "../client/build")));
 
+//Handle GET requests to /api route
 app.get("/api", (req, res) => {
   res.json({ message: "Data from the server" });
+});
+
+//All other GET requests not handled before will return React frontend
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../frontend/build", "index.html"));
 });
 
 app.listen(PORT, () => {
