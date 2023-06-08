542236c5f1aedf93848ed414a39b2a6601c5dab3
Sonnto <90372802+Sonnto@users.noreply.github.com>
2023-06-08 13:35:08 -0400

    Refactor App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,23 @@
 import "./App.css";
+import React from "react";
 import Banner from "./components/Banner";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Pixelate from "./components/Pixelate";
 
-function App() {
+export default function App() {
+  const [data, setData] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch("/api")
+      .then((res) => res.json())
+      .then((data) => setData(data.message));
+  }, []);
   return (
     <div>
       <Header />
       <main>
+        <p>{!data ? "Loading server test data..." : data}</p>
         <Banner />
         <Pixelate />
       </main>
@@ -16,5 +25,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
