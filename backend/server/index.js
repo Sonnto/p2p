def6ae4d714a7a39cfeac001f7d47b3efae69b23
Sonnto <90372802+Sonnto@users.noreply.github.com>
2023-06-11 18:55:45 -0400

    Create & connect database `pixelatedb`

--- backend/server/index.js
+++ backend/server/index.js
@@ -1,5 +1,15 @@
 const path = require("path");
 const express = require("express");
+const mysql = require("mysql");
+const pixelateImage = require("../../frontend/src/api/convert.js");
+
+//Manage pool of database connections
+const pool = mysql.createPool({
+  host: "localhost:8888",
+  user: "USERNAME",
+  password: "PASSWORD",
+  database: "pixelatedb",
+});
 
 const PORT = process.env.PORT || 1225;
 
@@ -10,6 +20,28 @@ app.use(express.static(path.resolve(__dirname, "../client/build")));
 
 //Handle GET requests to /api route
 app.get("/api", (req, res) => {
+  //Pixelate image and get pixelated image URL;
+  const pixelatedImageUrl = pixelateImage(req.query.image);
+  const instructions = "";
+  const segment = "";
+
+  //Store data in database
+  const data = {
+    originalImage: req.query.image,
+    pixelatedImage: pixelatedImageUrl,
+    instructions: instructions,
+    segment: segment,
+  };
+
+  pool.query("INSERT INTO pixelations SET ?", data, (error, results) => {
+    if (error) {
+      console.error("Error storing data in the database:", error);
+      res.status(500).json({ error: "Failed to store data" });
+    } else {
+      res.json({ message: "Data stored successfully" });
+    }
+  });
+
   res.json({ message: "Data from the server" });
 });
 
@@ -20,4 +52,13 @@ app.get("*", (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
+});
+
+process.on("SIGINT", () => {
+  pool.end((err) => {
+    if (err) {
+      console.error("Error closing the database connection:", err);
+    }
+    process.exit(0);
+  });
 });
