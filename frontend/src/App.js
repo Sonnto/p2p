1638b95e7395eb6bdee0a89f0a29366664ec1a87
Sonnto <90372802+Sonnto@users.noreply.github.com>
2023-06-13 22:20:03 -0400

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,18 +6,10 @@ import Header from "./components/Header";
 import Pixelate from "./components/Pixelate";
 
 export default function App() {
-  const [data, setData] = React.useState(null);
-
-  React.useEffect(() => {
-    fetch("/api")
-      .then((res) => res.json())
-      .then((data) => setData(data.message));
-  }, []);
   return (
     <div>
       <Header />
       <main>
-        <p>{!data ? "Loading server test data..." : data}</p>
         <Banner />
         <Pixelate />
       </main>
