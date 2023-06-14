f724dea622085e07cc875ba57b8177f6249a0b3c
Sonnto <90372802+Sonnto@users.noreply.github.com>
2023-06-13 22:21:31 -0400

    Add data to req.body

--- frontend/src/components/Pixelate.jsx
+++ frontend/src/components/Pixelate.jsx
@@ -1,18 +1,19 @@
 import React, { useState, useRef } from "react";
 import { pixelateImage } from "../api/convert";
+import { PDFDocument, StandardFonts, rgb } from "pdf-lib";
 
 const Pixelate = () => {
   const [selectedImage, setSelectedImage] = useState(null);
-  const [pixelSize, setPixelSize] = useState(10); // Default pixel size
-  const [pixelatedImage, setPixelatedImage] = useState(null); // Store the pixelated image data
-  const [errorMessage, setErrorMessage] = useState(""); // Error message state
+  const [pixelSize, setPixelSize] = useState(10);
+  const [convertedData, setConvertedData] = useState(null);
+  const [errorMessage, setErrorMessage] = useState("");
   const imageRef = useRef(null);
-  const frameSize = 400; // Adjust the frame size as needed
+  const frameSize = 400;
 
   const options = [
-    { label: "32x32 Brick (10\"x10\")", value: 10 },
-    { label: "64x64 Brick (20\"x20\")", value: 20 },
-    { label: "96x96 Lego Bricks (30\"x30\")", value: 30 },
+    { label: '32x32 Brick (10"x10")', value: 10 },
+    { label: '64x64 Brick (20"x20")', value: 20 },
+    { label: '96x96 Lego Bricks (30"x30")', value: 30 },
   ];
 
   const handleImageUpload = (event) => {
@@ -26,6 +27,12 @@ const Pixelate = () => {
     reader.readAsDataURL(file);
   };
 
+  const [segment, setSegment] = useState("");
+
+  const handleSegmentChange = (event) => {
+    setSegment(event.target.value);
+  };
+
   const handlePixelate = async () => {
     if (!selectedImage) {
       setErrorMessage("Error! Please upload an image before pixelating.");
@@ -36,7 +43,6 @@ const Pixelate = () => {
       const resizedImage = resizeImage();
       let adjustedPixelSize = pixelSize;
 
-      // Adjust pixel size for more detailed Lego pixels
       if (pixelSize === 30) {
         adjustedPixelSize = 10;
       } else if (pixelSize === 20) {
@@ -45,10 +51,61 @@ const Pixelate = () => {
         adjustedPixelSize = 30;
       }
 
-      const pixelatedImage = await pixelateImage(resizedImage, adjustedPixelSize);
+      const pixelatedImageResult = await pixelateImage(
+        resizedImage,
+        adjustedPixelSize
+      );
 
-      setPixelatedImage(pixelatedImage);
-      setErrorMessage(""); // This will clear any previous error message
+      setConvertedData(pixelatedImageResult);
+      setErrorMessage("");
+
+      const instructions = pixelatedImageResult.instructions;
+
+      // Create a new PDF document
+      const pdfDoc = await PDFDocument.create();
+      const page = pdfDoc.addPage();
+      const { width, height } = page.getSize();
+
+      // Embed a standard font
+      const font = await pdfDoc.embedFont(StandardFonts.Helvetica);
+
+      // Draw the instructions text
+      page.drawText(instructions, {
+        x: 50,
+        y: height - 100,
+        font,
+        size: 12,
+        color: rgb(0, 0, 0),
+      });
+
+      // Save the PDF document as a blob
+      const pdfBytes = await pdfDoc.save();
+      const instructionsFile = new File([pdfBytes], "instructions.pdf");
+
+      // Store data in database
+      const formData = new FormData();
+      formData.append("original_image", selectedImage);
+      formData.append("pixelated_image", pixelatedImageResult.pixelatedImage);
+      formData.append("instructions", instructionsFile);
+      formData.append("segment", segment);
+
+      console.log("originalImage:", selectedImage);
+      console.log("pixelatedImage:", pixelatedImageResult.pixelatedImage);
+      console.log("instructions:", instructions);
+      console.log("segment:", segment);
+
+      // Make a POST request to the backend endpoint
+      fetch("/api", {
+        method: "POST",
+        body: formData,
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          console.log("Data stored successfully:", data);
+        })
+        .catch((error) => {
+          console.error("Error storing data in the database:", error);
+        });
     } catch (error) {
       console.error(error);
     }
@@ -67,10 +124,8 @@ const Pixelate = () => {
 
   return (
     <div className="upload-section">
-      {/* Image upload UI */}
       <input type="file" onChange={handleImageUpload} />
 
-      {/* Image preview */}
       {selectedImage && (
         <div className="image-preview">
           <img
@@ -83,9 +138,8 @@ const Pixelate = () => {
         </div>
       )}
 
-      {/* Pixel size selection */}
       <div className="pixelsPerBrick">
-        <label htmlFor="pixelSize">Pixels per brick:</label>
+        <label htmlFor="pixelSize">Pixels per brick: </label>
         <select
           id="pixelSize"
           value={pixelSize}
@@ -99,22 +153,30 @@ const Pixelate = () => {
         </select>
       </div>
 
-      {/* Error message */}
+      <div>
+        <label htmlFor="segment">Segment: </label>
+        <input
+          type="text"
+          id="segment"
+          value={segment}
+          onChange={handleSegmentChange}
+        />
+      </div>
+
       {errorMessage && <p className="error-message">{errorMessage}</p>}
 
-      {/* Pixelate button */}
       <button
-        onClick={handlePixelate}
         className="p-2 text-xs md:text-sm bg-gradient-to-b from-orange-200 to-orange-400 border border-orange-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-orange-500 active:from-orange-500 text-white"
+        onClick={handlePixelate}
       >
+        {" "}
         Pixelate!
       </button>
 
-      {/* Display the pixelated image */}
-      {pixelatedImage && (
+      {convertedData && (
         <div>
           <h3>Pixelated Image:</h3>
-          <img src={pixelatedImage} alt="pixelated" />
+          <img src={convertedData.pixelatedImage} alt="pixelated" />
         </div>
       )}
     </div>
@@ -122,108 +184,3 @@ const Pixelate = () => {
 };
 
 export default Pixelate;
-
-
-
-// import React, { useState, useRef } from "react";
-// import { pixelateImage } from "../api/convert";
-
-// const Pixelate = () => {
-//   const [selectedImage, setSelectedImage] = useState(null);
-//   const [pixelSize, setPixelSize] = useState(10); // Default pixel size
-//   const [pixelatedImage, setPixelatedImage] = useState(null); // Store the pixelated image data
-//   const imageRef = useRef(null);
-//   const frameSize = 300; // Adjust the frame size as needed
-//   //instead of inputting number of pixels manually, users have 3 choices
-//   const options = [
-//     { label: "32x32 Brick (10\"x10\")", value: 10 },
-//     { label: "64x64 Brick (20\"x20\")", value: 20 },
-//     { label: "96x96 Lego Bricks (30\"x30\")", value: 30 },
-//   ];
-
-
-//   const handleImageUpload = (event) => {
-//     const file = event.target.files[0];
-//     const reader = new FileReader();
-
-//     reader.onload = (e) => {
-//       setSelectedImage(e.target.result);
-//     };
-
-//     reader.readAsDataURL(file);
-//   };
-
-//   const handlePixelate = async () => {
-//     if (!selectedImage) return;
-
-//     try {
-//       const resizedImage = resizeImage();
-//       const pixelatedImage = await pixelateImage(resizedImage, pixelSize);
-
-//       setPixelatedImage(pixelatedImage);
-//     } catch (error) {
-//       console.error(error);
-//     }
-//   };
-
-//   const resizeImage = () => {
-//     const canvas = document.createElement("canvas");
-//     canvas.width = frameSize;
-//     canvas.height = frameSize;
-
-//     const context = canvas.getContext("2d");
-//     context.drawImage(imageRef.current, 0, 0, frameSize, frameSize);
-
-//     return canvas.toDataURL("image/jpeg");
-//   };
-
-//   return (
-//     <div>
-//       {/* Image upload UI */}
-//       <input type="file" onChange={handleImageUpload} />
-
-//       {/* Image preview */}
-//       {selectedImage && (
-//         <div className="image-preview">
-//           <img
-//             ref={imageRef}
-//             src={selectedImage}
-//             alt="pixelate"
-//             width={frameSize}
-//             height={frameSize}
-//           />
-//         </div>
-//       )}
-
-//       {/* Pixel size selection */}
-//       <div>
-//         <label htmlFor="pixelSize">Pixels per brick:</label>
-//         <input
-//           type="number"
-//           id="pixelSize"
-//           value={pixelSize}
-//           onChange={(e) => setPixelSize(Number(e.target.value))}
-//           min={1}
-//         />
-//       </div>
-
-//       {/* Pixelate button */}
-//       <button
-//         onClick={handlePixelate}
-//         className="p-2 text-xs md:text-sm bg-gradient-to-b from-orange-200 to-orange-400 border border-orange-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-orange-500 active:from-orange-500 text-white"
-//       >
-//         Pixelate!
-//       </button>
-
-//       {/* Display the pixelated image */}
-//       {pixelatedImage && (
-//         <div>
-//           <h3>Pixelated Image:</h3>
-//           <img src={pixelatedImage} alt="pixelated" />
-//         </div>
-//       )}
-//     </div>
-//   );
-// };
-
-// export default Pixelate;
